c9920653990e813a3128197b7049c0d4f5078e4d
mokahontas <alexisfelicilda@yahoo.com>
2016-06-28 21:15:48 -1000

    added boat and hula dancer

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,17 @@
+console.log($);
 
+$(document).ready(function () {
+$('.huladancer').on("click", function (e) {
+    this.notice = $('<div>')
+        .addClass('notice')
+        .css({
+            top: $(this).offset().top + 15,
+            left: $(this).offset().left + 20
+        }).text($(this).data('notice'))
+        .appendTo(this);
+}, function () {
+    this.notice.remove();
+console.log(this);
+});
+
+});
