6b65ef69bdb27ddd1b55acb6f7a14f387954a89f
mokahontas <alexisfelicilda@yahoo.com>
2016-06-28 23:14:21 -1000

    added boat and jquery

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,10 @@
-console.log($);
-
 $(document).ready(function () {
-$('.huladancer').on("click", function (e) {
-    this.notice = $('<div>')
-        .addClass('notice')
-        .css({
-            top: $(this).offset().top + 15,
-            left: $(this).offset().left + 20
-        }).text($(this).data('notice'))
-        .appendTo(this);
-}, function () {
-    this.notice.remove();
-console.log(this);
+$('.profile').hide();
+$('.projects').hide();
+$('#image').click(function (e) {
+    $('.profile').toggle();
 });
-
+$('.boat').click(function (e) {
+    $('.projects').toggle();
+});
 });
