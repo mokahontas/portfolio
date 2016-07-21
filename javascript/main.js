3d38d1a9c8ad0aedcfbca29d520d56bcbb29a5c9
mokahontas <alexisfelicilda@yahoo.com>
2016-07-20 21:35:28 -1000

    add profile

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-$('.profile').hide();
+$('#profile').hide();
 $('.projects').hide();
 $('#image').click(function (e) {
-    $('.profile').toggle();
+    $('#profile').toggle();
 });
 $('.boat').click(function (e) {
     $('.projects').toggle();
