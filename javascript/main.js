af327d2a86f8361a03bbfa9aef15d4a3488690dd
Lex <mokahontas808@gmail.com>
2016-07-25 21:40:53 -1000

    merge

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,11 @@
 $(document).ready(function () {
 $('#profile').hide();
 $('.projects').hide();
-$('.click-here').hide();
-$('#image').hover(function (e) {
-	$('.click-here').fadeToggle(1000);	
-});
 $('#image').click(function (e) {
     $('#profile').fadeToggle(1000);
 });
 $('.close').click(function (e) {
 	$('#profile').fadeToggle(1000);
-	
 });
 $('.boat').click(function (e) {
     $('.projects').fadeToggle(1000);
